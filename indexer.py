0f490b10de460d7fc5d457f1e0e69ed59d509cfd
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-01-07 20:57:19 +0200

    small change in indexer.py update_tweets_postings

--- indexer.py
+++ indexer.py
@@ -11,7 +11,7 @@ class Indexer:
     # You can change the internal implementation as you see fit.
     def __init__(self, config):
         self.config = config
-        self.terms_index = dict()  # maps terms to their postings in disc
+        self.terms_index = dict()  # maps terms to their postings
 
         self.tweets_postings_file = dict()  # maps tweets to their relevant information
 
@@ -21,7 +21,11 @@ class Indexer:
     # DO NOT MODIFY THIS SIGNATURE
     # You can change the internal implementation as you see fit.
     def add_new_doc(self, document):
-        # update the number of docs in the corpus
+        """
+
+        :param document:
+        :return:
+        """
         self.doc_id += 1
 
         # index every term in document and return the term who appeared most frequently.
@@ -65,6 +69,12 @@ class Indexer:
             self.insert_term_to_terms_index(term, term_freq_in_tweet)
 
     def insert_term_to_terms_index(self, term, term_freq_in_tweet):
+        """
+
+        :param term:
+        :param term_freq_in_tweet:
+        :return:
+        """
         self.terms_index[term][0] += 1  # df
         self.terms_index[term][1] += term_freq_in_tweet  # term_freq_in_corpus
         self.terms_index[term][2].add(self.doc_id)  # set of docs
@@ -73,6 +83,12 @@ class Indexer:
     # after indexing the terms, updates the tweet's information in the indexer.
     # if the postings file for the tweets is too large, it is moved to the disc and emptied in memory.
     def update_tweets_information(self, document, max_tf):
+        """
+
+        :param document:
+        :param max_tf:
+        :return:
+        """
         self.tweets_postings_file[self.doc_id] = [document.tweet_id, max_tf, document.term_doc_dictionary]
 
         # move tweets postings to disc if needed
@@ -80,8 +96,13 @@ class Indexer:
             self.dump_tweet_postings_to_disc()
 
 
-    #  calculates whether postings file is too large
     def postings_too_large(self, current_postings_size, optimal_file_size):
+        """
+
+        :param current_postings_size:
+        :param optimal_file_size:
+        :return:
+        """
         return current_postings_size % optimal_file_size == optimal_file_size - 1
 
     # DO NOT MODIFY THIS SIGNATURE
@@ -104,6 +125,10 @@ class Indexer:
         """
         utils.save_obj(obj=self.terms_index, name=fn, path='')
 
+    def post_process(self):
+        self.dump_tweet_postings_to_disc()
+        self.update_tweets_postings()
+
     def update_tweets_postings(self):
         """
 
@@ -111,30 +136,38 @@ class Indexer:
         """
         for i in range(self.tweets_postings_counter):
             tweets_postings_file = utils.load_obj(str(i+1), self.config.get_tweets_postings_path())
-            for tweet in tweets_postings_file.keys():
-                sigma_wij = 0
-                updated_term_doc_dict = dict()
-                tweet_posting = tweets_postings_file[tweet]
-                term_doc_dict = tweet_posting[2]
-                for term in term_doc_dict.keys():
-                    lower_case_term = term[0].lower() + term[1:]
-                    # dont write to new dict = delete,  when term_freq_in_corpus == 1.
-                    # deletion from terms_index (del)  and postings (not saved in updated, "continue")
-                    if term in self.terms_index.keys() and self.terms_index[term][1] == 1:
-                        del self.terms_index[term]
-                        continue
-                    # calculates tf-idf with term_freq_in_tweet, max_tf, df and N. the upper-case term
-                    # is removed and replaced later with an entry of its' lower_case form as key and a tf-idf value
-                    elif term[0].isupper() and lower_case_term in self.terms_index.keys():
-                        df = self.terms_index[lower_case_term][0]
-                    else:
-                        df = self.terms_index[term][0]
-                    tf_idf = self.calculate_tf_idf(term_doc_dict[term], tweet_posting[1],df, self.doc_id)
-                    updated_term_doc_dict[lower_case_term] = tf_idf
-                    sigma_wij += math.pow(tf_idf,2)
-                tweet_posting[1] = sigma_wij
-                tweet_posting[2] = updated_term_doc_dict
+            for doc_id in tweets_postings_file.keys():
+                tweet_posting = tweets_postings_file[doc_id]
+                self.update_single_tweet_postings(tweet_posting)
             utils.save_obj(tweets_postings_file, str(i+1), self.config.get_tweets_postings_path())
+
+    def update_single_tweet_postings(self, tweet_posting):
+        """
+
+        :param tweet_posting:
+        :return:
+        """
+        sigma_wij = 0
+        updated_term_doc_dict = dict()
+        term_doc_dict = tweet_posting[2]
+        for term in term_doc_dict.keys():
+            lower_case_term = term[0].lower() + term[1:]
+            # dont write to new dict = delete,  when term_freq_in_corpus == 1.
+            # deletion from terms_index (del)  and postings (not saved in updated, "continue")
+            if term in self.terms_index.keys() and self.terms_index[term][1] == 1:
+                del self.terms_index[term]
+                continue
+            # calculates tf-idf with term_freq_in_tweet, max_tf, df and N. the upper-case term
+            # is removed and replaced later with an entry of its' lower_case form as key and a tf-idf value
+            elif term[0].isupper() and lower_case_term in self.terms_index.keys():
+                df = self.terms_index[lower_case_term][0]
+            else:
+                df = self.terms_index[term][0]
+            tf_idf = self.calculate_tf_idf(term_doc_dict[term], tweet_posting[1], df, self.doc_id)
+            updated_term_doc_dict[lower_case_term] = tf_idf
+            sigma_wij += math.pow(tf_idf, 2)
+        tweet_posting[1] = sigma_wij
+        tweet_posting[2] = updated_term_doc_dict
 
     def calculate_tf_idf(self, term_freq_in_tweet, max_tf, df, N):
         """
@@ -158,9 +191,6 @@ class Indexer:
     def get_tweets_postings_counter(self):
         return self.tweets_postings_counter
 
-    def post_process(self):
-        self.dump_tweet_postings_to_disc()
-        self.update_tweets_postings()
 
     def dump_tweet_postings_to_disc(self):
         #  write the postings to disc and empty the dictionary in memory.
@@ -168,3 +198,4 @@ class Indexer:
         utils.save_obj(self.tweets_postings_file, str(self.tweets_postings_counter),
                        self.config.get_tweets_postings_path())
         self.tweets_postings_file.clear()
+
---
+++ search_engine_best.py
@@ -0,0 +1,140 @@
+import pandas as pd
+
+import utils
+from parser_classes.parsers.parser_module import Parse
+from indexer import Indexer
+from searchers.wordnet_searcher import Searcher
+from configuration import ConfigClass
+
+
+# DO NOT CHANGE THE CLASS NAME
+class SearchEngine:
+
+    # DO NOT MODIFY THIS SIGNATURE
+    # You can change the internal implementation, but you must have a parser and an indexer.
+    def __init__(self, config=None):
+        self._config = config
+        self._parser = Parse(config.get_stemming())
+        self._indexer = Indexer(config)
+        self._model = None
+
+    # DO NOT MODIFY THIS SIGNATURE
+    # You can change the internal implementation as you see fit.
+    def build_index_from_parquet(self, fn):
+        """
+        Reads parquet file and passes it to the parser, then indexer.
+        Input:
+            fn - path to parquet file
+        Output:
+            No output, just modifies the internal _indexer object.
+        """
+        df = pd.read_parquet(fn, engine="pyarrow")
+        documents_list = df.values.tolist()
+        # Iterate over every document in the file
+        number_of_documents = 0
+        for idx, document in enumerate(documents_list):
+            # parse the document
+            parsed_document = self._parser.parse_doc(document)
+            number_of_documents += 1
+            # index the document data
+            self._indexer.add_new_doc(parsed_document)
+        print('Finished parsing and indexing. commencing post processing...')
+        self._indexer.post_process()
+        print('Finished post processing.')
+        # self._indexer.save_index(fn=self._indexer.get_config().get_index_name())
+
+    # DO NOT MODIFY THIS SIGNATURE
+    # You can change the internal implementation as you see fit.
+    def load_index(self, fn):
+        """
+        Loads a pre-computed index (or indices) so we can answer queries.
+        Input:
+            fn - file name of pickled index.
+        """
+        self._indexer.load_index(fn)
+
+    # DO NOT MODIFY THIS SIGNATURE
+    # You can change the internal implementation as you see fit.
+    def load_precomputed_model(self, model_dir=None):
+        """
+        Loads a pre-computed model (or models) so we can answer queries.
+        This is where you would load models like word2vec, LSI, LDA, etc. and 
+        assign to self._model, which is passed on to the searcher at query time.
+        """
+        pass
+
+    # DO NOT MODIFY THIS SIGNATURE
+    # You can change the internal implementation as you see fit.
+    def search(self, query):
+        """ 
+        Executes a query over an existing index and returns the number of 
+        relevant docs and an ordered list of search results.
+        Input:
+            query - string.
+        Output:
+            A tuple containing the number of relevant search results, and 
+            a list of tweet_ids where the first element is the most relevant
+            and the last is the least relevant result.
+        """
+        searcher = Searcher(self._parser, self._indexer, model=self._model)
+        return searcher.search(query)
+
+    def test_build_index_from_parquet(self, fn): #todo delete when submitting
+        """
+        Reads parquet file and passes it to the parser, then indexer.
+        Input:
+            fn - path to parquet file
+        Output:
+            No output, just modifies the internal _indexer object.
+        """
+        # df = pd.read_parquet(fn, engine="pyarrow")
+        # documents_list = df.values.tolist()
+        # # Iterate over every document in the file
+        document_1 = ['12809215422436591023', 'Wed Jul 08 17:47:48 +0000 2020', "naor asd banana tom",
+                      '{"https://t.co/4A5TDSyjoY":"https://twitter.com/i/web/status/1280921542243659776"}',
+                      '[[117,140]]', None, None, None, None, None, None, None, None, None]
+        document_2 = ['1280921542243659110', 'Wed Jul 08 17:47:48 +0000 2020',
+                      "banana morty rick Baby asd BanaNa fall Ping Ball",
+                      '{"https://t.co/4A5TDSyjoY":"https://twitter.com/i/web/status/1280921542243659776"}',
+                      '[[117,140]]', None, None, None, None, None, None, None, None, None]
+        document_3 = ['1280921542243659101', 'Wed Jul 08 17:47:48 +0000 2020',
+                      "Banana asd rick banana banana banana banana banana banana",
+                      '{"https://t.co/4A5TDSyjoY":"https://twitter.com/i/web/status/1280921542243659776"}',
+                      '[[117,140]]', None, None, None, None, None, None, None, None, None]
+        document_4 = ['128092154224365910222', 'Wed Jul 08 17:47:48 +0000 2020',
+                      "lalala hello lalala",
+                      '{"https://t.co/4A5TDSyjoY":"https://twitter.com/i/web/status/1280921542243659776"}',
+                      '[[117,140]]', None, None, None, None, None, None, None, None, None]
+        documents_list = [document_1, document_2,document_3, document_4]
+        number_of_documents = 0
+        for idx, document in enumerate(documents_list):
+            # parse the document
+            parsed_document = self._parser.parse_doc(document)
+            number_of_documents += 1
+            # index the document data
+            self._indexer.add_new_doc(parsed_document)
+
+            if number_of_documents == 1:
+                self._indexer.dump_tweet_postings_to_disc()
+        print('Finished parsing and indexing. commencing post processing...')
+        self._indexer.post_process()
+        print('Finished post processing.')
+        # self._indexer.save_index(self._indexer.config.get_stemming_dir_path())
+
+def main():
+    config = ConfigClass()
+
+    # create parent directories for postings
+    utils.create_parent_dir(config.get_stemming_dir_path())
+    utils.create_parent_dir(config.get_terms_postings_path())
+    utils.create_parent_dir(config.get_tweets_postings_path())
+
+    se = SearchEngine(config)
+    se.build_index_from_parquet(config.get_corpusPath())
+
+    # start_time = time.time()
+    n_res,res = se.search('Bill Gates')
+    # print('query returned in: ' + str(time.time() -start_time) + ' seconds')
+    print("Tweet id: {}".format(res[0]))
+
+
